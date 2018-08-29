c59ebd5c8658fd63b1a2c042d54a37bd7b5c12e4
LfLanzoni <lflanzoni@gmail.com>
2018-08-29 13:56:23 -0300

    Listview de reclamos y cambio de estado v1.0

--- app/src/main/java/ar/android/lflanzoni/reclamosonline/ListaReclamosActivity.java
+++ app/src/main/java/ar/android/lflanzoni/reclamosonline/ListaReclamosActivity.java
@@ -2,12 +2,41 @@ package ar.android.lflanzoni.reclamosonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemLongClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import ar.android.lflanzoni.reclamosonline.modelo.Reclamo;
+import ar.android.lflanzoni.reclamosonline.modelo.ReclamoDAO;
+import ar.android.lflanzoni.reclamosonline.modelo.ReclamoDAOsql;
 
 public class ListaReclamosActivity extends AppCompatActivity {
+    ReclamoDAO reclamoDAO;
+    ListView listaReclamos;
+    ArrayAdapter<Reclamo> adaptadorReclamos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lista_reclamos);
+        listaReclamos = (ListView) findViewById(R.id.listaReclamos);
+        reclamoDAO = new ReclamoDAOsql(this);
+        adaptadorReclamos = new ArrayAdapter<Reclamo>(this,android.R.layout.simple_list_item_1,reclamoDAO.listarReclamos());
+        listaReclamos.setAdapter(adaptadorReclamos);
+
+        this.listaReclamos.setOnItemLongClickListener(new OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int position, long id) {
+                int itemPosition = position;
+                Reclamo itemValue = (Reclamo) listaReclamos.getItemAtPosition(position);
+                itemValue.setResuelto(true);
+                reclamoDAO.actualizar(itemValue);
+                adaptadorReclamos.notifyDataSetChanged();
+                return false;
+            }
+        });
     }
 }
